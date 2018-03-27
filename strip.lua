e65de9ef798a7b110cf551251fb3af08c15d5800
Erlkoenigs <37835178+Erlkoenigs@users.noreply.github.com>
2018-03-27 21:03:57 +0200

    added contents
    will now create a fixed number of strips without digging out ores

--- strip.lua
+++ strip.lua
@@ -1,3 +1,7 @@
+local stripDirection = "right" --New strips will be created to the "stripDirection" of the Startingpoint of the turtle
+local stripLength = 200
+local numberOfStrips = 5
+
 function Refuel()
     turtle.select(1)
     if not turtle.refuel(1) then
@@ -8,19 +12,64 @@ function Refuel()
     end
 end
 
-function StripForward()
-    for i=0, i<200 do
-        while turtle.detect() do
-            turtle.dig()
-            os.sleep(0.5)
-        turtle.forward()
-        while turtle.detectUp() do
-            turtle.digUp()
-            os.sleep(0.5)
+--First run through the strip. Needs a second run to clear out possible gravel or sand that has fallen down behind the turtle(StripBack)
+function StripForward(blocks)
+    local steps = 0
+    while steps<blocks do
+        turtle.dig()        
+        if turtle.forward() then
+            steps = steps+1
+        end
+        turtle.digUp()
     end
 end
 
-function StripBack()
+--Similar to StripForward but with timers to wait for blocks to fall down
+function StripBack(blocks)
+    local steps = 0
+    while steps<blocks do
+        turtle.dig()
+        os.sleep(0.5)
+        if turtle.forward() then
+            steps = steps+1
+        end
+    end
+end
+
+function Strip(length)
+    StripForward(length)
     turtle.turnLeft()
     turtle.turnLeft()
-    for i=0, i<200
+    StripBack(length)
+    turtle.turnLeft()
+    turtle.turnLeft()
+end
+
+function Reposition(direction)
+    if direction == "right" then
+        turtle.turnRight()
+    elseif direction == "left" then
+        turtle.turnLeft()
+    else
+        print("Invalid direction. Use left or right")
+    end
+    while steps<3 do
+        turtle.dig()
+        if turtle.forward() then
+            steps = steps+1
+        end
+    end
+    if direction == "right" then
+        turtle.turnLeft()
+    elseif direction == "left" then
+        turtle.turnRight()
+    else
+        print("Invalid direction. Use left or right")
+    end
+end
+
+while strips<numberOfStrips do
+    Strip(stripLength)
+    Reposition(stripDirection)
+    strips = strips+1
+end
