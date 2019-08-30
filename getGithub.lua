4af019fb2b87a80bd4ec6b8bfe92ed6785085d67
Erlkoenigs <37835178+Erlkoenigs@users.noreply.github.com>
2019-08-30 14:58:54 +0200

    don't run the downloaded program

--- getGithub.lua
+++ getGithub.lua
@@ -1,12 +1,12 @@
-local url = "https://raw.githubusercontent.com/Erlkoenigs/computercraft/master/strip.lua"
+print("program to download:")
+local program = read()
+local url = "https://raw.githubusercontent.com/Erlkoenigs/computercraft/master/"..program..".lua"    
 local website = http.get(url)
 if website then
     print("received")
     github_file = website.readAll()
-    local h = fs.open("stripProgram.lua","w")
+    local h = fs.open(program..".lua","w")
     h.write(github_file)
     h.close()
-    print("run:")
-    shell.run("stripProgram.lua")
 end
 website.close()
