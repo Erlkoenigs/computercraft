cd726060011d08b7948042bb626ca2bacaf784fd
Erlkoenigs <37835178+Erlkoenigs@users.noreply.github.com>
2019-09-04 15:39:54 +0200

    added comments

--- farmEnderLilly.lua
+++ farmEnderLilly.lua
@@ -1,15 +1,20 @@
 --farm one ender lilly once it's ready, put the pearl in a chest to the right and replant the seed
+--ender lilly has to be planted in front of the turtle
+--a chest for the ender pearls has to be on the right side of the turtle
 while true do
     local s,d=turtle.inspect()
     if s then
         if d.state.growth==7 then
         turtle.dig()
+            --find lilly seed and pearl in inventory
             for i=1,16 do
                 turtle.select(i)
                 local data=turtle.getItemDetail()
                 if data~=nil then
+                    --plant the seed
                     if data.name=="extrautils2:enderlilly" then
                         turtle.place()
+                    --put the pearl in the chest
                     elseif data.name=="minecraft:ender_pearl" then
                         turtle.turnRight()
                         while not turtle.drop() do
