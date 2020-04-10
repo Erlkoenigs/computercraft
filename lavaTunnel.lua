a5ba2bbcb1d7f7f99ccf5c79752932efd9c71ee5
fabi <faffa118@gmail.com>
2020-04-10 10:34:19 +0200

    take command line arguments

--- lavaTunnel.lua
+++ lavaTunnel.lua
@@ -1,8 +1,16 @@
 --This program is meant for the computercraft turtle
 --It moves forward while placing blocks above, left and right of itself
---It creates a 1x2 tunnel, which can be used to get rid of a lava flow
-print("Input length of the tunnel:")
-local length = read()
+--This creates a 1x2 tunnel, which can be used to get rid of a lava flow
+--Fuel in slot 1, material to place in slot 2
+
+--give length as command line argument
+tArgs={...}
+if #tArgs > 0 and type(tonumber(tArgs[1])) == "number" then
+    length = tArgs[1]
+else
+    print("Input length of the tunnel:")
+    local length = read()
+end
 
 --places a blocks all around a 1x2 area
 function place()
